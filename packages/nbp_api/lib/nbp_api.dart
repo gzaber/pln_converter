9056cf84cc1b4cd8062afad2e94c13a60372c303
Grzesiek <gzaber@tlen.pl>
2022-07-06 19:19:44 +0200

    Clear package folder

--- packages/nbp_api/lib/nbp_api.dart
+++ packages/nbp_api/lib/nbp_api.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library nbp_api;
 
-export 'src/nbp_api_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/nbp_api.dart';
