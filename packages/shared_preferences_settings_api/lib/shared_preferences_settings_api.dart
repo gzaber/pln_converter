dfc3b1d1227377f646e8690d1b6ed0a94f19ff9c
Grzesiek <gzaber@tlen.pl>
2022-07-09 16:07:33 +0200

    Clean up shared_preferences_settings_api package

--- packages/shared_preferences_settings_api/lib/shared_preferences_settings_api.dart
+++ packages/shared_preferences_settings_api/lib/shared_preferences_settings_api.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library shared_preferences_settings_api;
 
-export 'src/shared_preferences_settings_api_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/shared_preferences_settings_api.dart';
