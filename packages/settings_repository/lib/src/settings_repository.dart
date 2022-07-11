2bb2dc66cb8598b2c371efc72ca7f4bd862a233d
Grzesiek <gzaber@tlen.pl>
2022-07-11 20:04:30 +0200

    Revert "Add Settings model to export"
    This reverts commit c9d2eea6373c45d4a14ef56a6f9ac435ef7a05c1.

--- packages/settings_repository/lib/src/settings_repository.dart
+++ packages/settings_repository/lib/src/settings_repository.dart
@@ -1,14 +1,1 @@
-import 'package:settings_api/settings_api.dart';
 
-class SettingsRepository {
-  const SettingsRepository({
-    required SettingsApi settingsApi,
-  }) : _settingsApi = settingsApi;
-
-  final SettingsApi _settingsApi;
-
-  Settings? getSettings() => _settingsApi.getSettings();
-
-  Future<void> saveSettings(Settings settings) async =>
-      await _settingsApi.saveSettings(settings);
-}
