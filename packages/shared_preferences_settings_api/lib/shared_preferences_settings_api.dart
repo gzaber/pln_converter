f3488d83a8727966049a684541f035d64b332384
Grzesiek <gzaber@tlen.pl>
2022-07-11 18:43:53 +0200

    Add export SharedPreferences from SharedPreferencesSettingsApi

--- packages/shared_preferences_settings_api/lib/shared_preferences_settings_api.dart
+++ packages/shared_preferences_settings_api/lib/shared_preferences_settings_api.dart
@@ -1,3 +1,5 @@
 library shared_preferences_settings_api;
 
+export 'package:shared_preferences/shared_preferences.dart'
+    show SharedPreferences;
 export 'src/shared_preferences_settings_api.dart';
