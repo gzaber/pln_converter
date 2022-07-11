8b98b7da2169cbdc332bb7e844836322a071f3c4
Grzesiek <gzaber@tlen.pl>
2022-07-11 20:34:08 +0200

    Clean up exchange_rates_repository package folder

--- packages/exchange_rates_repository/example/exchange_rates_repository_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:exchange_rates_repository/exchange_rates_repository.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- packages/exchange_rates_repository/test/exchange_rates_repository_test.dart
+++ packages/exchange_rates_repository/test/exchange_rates_repository_test.dart
@@ -1,16 +1,5 @@
-import 'package:exchange_rates_repository/exchange_rates_repository.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
+  group('ExchangeRatesRepository', () {});
 }
