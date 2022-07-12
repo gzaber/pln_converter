5490f7df6254c68f3e36d39b7b5704050d3aa7ab
Grzesiek <gzaber@tlen.pl>
2022-07-12 21:11:21 +0200

    Create ExchangeRatesRepository

--- packages/exchange_rates_repository/lib/src/exchange_rates_repository.dart
+++ packages/exchange_rates_repository/lib/src/exchange_rates_repository.dart
@@ -1,1 +1,48 @@
-class ExchangeRatesRepository {}
+import 'package:exchange_rates_repository/exchange_rates_repository.dart';
+import 'package:nbp_api/nbp_api.dart' hide Currency;
+
+class ExchangeRatesRepository {
+  ExchangeRatesRepository({NbpApiClient? nbpApiClient})
+      : _nbpApiClient = nbpApiClient ?? NbpApiClient();
+
+  final NbpApiClient _nbpApiClient;
+
+  Future<Currency> getCurrency(String table, String code) async {
+    final currency = await _nbpApiClient.getCurrency(table, code);
+
+    return Currency(
+      name: currency.currency,
+      code: currency.code,
+      table: currency.table,
+      rate: currency.rates.first.mid,
+    );
+  }
+
+  Future<List<Currency>> getCurrencies() async {
+    final tableA = await _nbpApiClient.getTable('A');
+    final tableB = await _nbpApiClient.getTable('B');
+
+    final currenciesA = tableA.rates
+        .map(
+          (cr) => Currency(
+            name: cr.currency,
+            code: cr.code,
+            table: tableA.table,
+            rate: cr.mid,
+          ),
+        )
+        .toList();
+    final currenciesB = tableB.rates
+        .map(
+          (cr) => Currency(
+            name: cr.currency,
+            code: cr.code,
+            table: tableB.table,
+            rate: cr.mid,
+          ),
+        )
+        .toList();
+
+    return [...currenciesA, ...currenciesB];
+  }
+}
