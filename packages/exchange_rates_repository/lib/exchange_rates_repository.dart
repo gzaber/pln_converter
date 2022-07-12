4f0fe59726de54d8a4e779f6761d5e21f920b6d9
Grzesiek <gzaber@tlen.pl>
2022-07-12 20:10:20 +0200

    Create Currency model, add it to export from package

--- packages/exchange_rates_repository/lib/exchange_rates_repository.dart
+++ packages/exchange_rates_repository/lib/exchange_rates_repository.dart
@@ -1,3 +1,4 @@
 library exchange_rates_repository;
 
 export 'src/exchange_rates_repository.dart';
+export 'src/models/models.dart';
---
+++ packages/exchange_rates_repository/lib/src/models/currency.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+class Currency extends Equatable {
+  Currency({
+    required this.name,
+    required this.code,
+    required this.table,
+    required this.rate,
+  });
+
+  final String name;
+  final String code;
+  final String table;
+  final String rate;
+
+  @override
+  List<Object?> get props => [name, code, table, rate];
+}
