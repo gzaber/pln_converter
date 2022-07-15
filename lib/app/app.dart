d7adc321bbac71da4f4adc051cc28669ca54d920
Grzesiek <gzaber@tlen.pl>
2022-07-15 18:47:58 +0200

    Add bloc provider with AppCubit to App

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:exchange_rates_repository/exchange_rates_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pln_converter/app/cubit/app_cubit.dart';
 import 'package:pln_converter/home/home.dart';
 import 'package:settings_repository/settings_repository.dart';
 
@@ -21,7 +22,12 @@ class App extends StatelessWidget {
         RepositoryProvider.value(value: settingsRepository),
         RepositoryProvider.value(value: exchangeRatesRepository),
       ],
-      child: const AppView(),
+      child: BlocProvider(
+        create: (context) => AppCubit(
+          settingsRepository: context.read<SettingsRepository>(),
+        )..readSettings(),
+        child: const AppView(),
+      ),
     );
   }
 }
