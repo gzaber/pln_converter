800deebdf3629ea612275e44256c778b929a6225
Grzesiek <gzaber@tlen.pl>
2022-07-18 20:44:34 +0200

    main - add WidgetsFlutterBinding.ensureInitialized()

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:settings_repository/settings_repository.dart';
 import 'package:shared_preferences_settings_api/shared_preferences_settings_api.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final prefs = await SharedPreferences.getInstance();
   final settingsApi = SharedPreferencesSettingsApi(prefs: prefs);
   final settingsRepository = SettingsRepository(settingsApi: settingsApi);
