d5d2d918bd8c2244c19c4f90a3784c5d3c2ccce7
Grzesiek <gzaber@tlen.pl>
2022-07-22 22:08:00 +0200

    Create tests for ExchangeRatesCubit

---
+++ test/exchange_rates/cubit/exchange_rates_cubit_test.dart
@@ -0,0 +1,132 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:exchange_rates_repository/exchange_rates_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:pln_converter/exchange_rates/cubit/exchange_rates_cubit.dart';
+
+class MockExchangeRatesRepository extends Mock
+    implements ExchangeRatesRepository {}
+
+void main() {
+  group('ExchangeRatesCubit', () {
+    late ExchangeRatesRepository repository;
+
+    setUp(() {
+      repository = MockExchangeRatesRepository();
+    });
+
+    ExchangeRatesCubit buildCubit() => ExchangeRatesCubit(repository);
+
+    group('constructor', () {
+      test('works properly', () {
+        expect(() => buildCubit(), returnsNormally);
+      });
+
+      test('initial state is correct', () {
+        expect(buildCubit().state, equals(const ExchangeRatesState()));
+      });
+    });
+
+    group('getExchangeRates', () {
+      final usd = Currency(
+          name: 'dolar amerykański', code: 'USD', table: 'A', rate: 4.8284);
+
+      blocTest<ExchangeRatesCubit, ExchangeRatesState>(
+        'emits state with success status and exchange rates list when successfully'
+        'fetched from API',
+        setUp: () {
+          when(() => repository.getCurrencies()).thenAnswer((_) async => [usd]);
+        },
+        build: () => buildCubit(),
+        act: (cubit) => cubit.getExchangeRates(),
+        expect: () => [
+          const ExchangeRatesState(status: ExchangeRatesStatus.loading),
+          ExchangeRatesState(
+            status: ExchangeRatesStatus.success,
+            exchangeRates: [usd],
+          ),
+        ],
+      );
+
+      blocTest<ExchangeRatesCubit, ExchangeRatesState>(
+        'emits state with failure status and error message when exception occurs',
+        setUp: () {
+          when(() => repository.getCurrencies())
+              .thenThrow(Exception('failure'));
+        },
+        build: () => buildCubit(),
+        act: (cubit) => cubit.getExchangeRates(),
+        expect: () => const [
+          ExchangeRatesState(status: ExchangeRatesStatus.loading),
+          ExchangeRatesState(
+            status: ExchangeRatesStatus.failure,
+            errorMessage: 'Exception: failure',
+          ),
+        ],
+      );
+    });
+
+    group('turnOnSearch', () {
+      blocTest<ExchangeRatesCubit, ExchangeRatesState>(
+        'emits state with search status and empty filtered list',
+        build: () => buildCubit(),
+        act: (cubit) => cubit.turnOnSearch(),
+        expect: () => const [
+          ExchangeRatesState(
+            status: ExchangeRatesStatus.search,
+            filteredList: [],
+          ),
+        ],
+      );
+    });
+
+    group('turnOffSearch', () {
+      blocTest<ExchangeRatesCubit, ExchangeRatesState>(
+        'emits state with success status',
+        build: () => buildCubit(),
+        act: (cubit) => cubit.turnOffSearch(),
+        expect: () => const [
+          ExchangeRatesState(
+            status: ExchangeRatesStatus.success,
+          ),
+        ],
+      );
+    });
+
+    group('search', () {
+      final usd = Currency(
+          name: 'dolar amerykański', code: 'USD', table: 'A', rate: 4.8284);
+      final aud = Currency(
+          name: 'dolar australijski', code: 'AUD', table: 'A', rate: 3.2449);
+
+      blocTest<ExchangeRatesCubit, ExchangeRatesState>(
+        'emits filtered list',
+        seed: () => ExchangeRatesState(
+            status: ExchangeRatesStatus.search,
+            exchangeRates: [aud, usd],
+            filteredList: const []),
+        build: () => buildCubit(),
+        act: (cubit) => cubit.search('ame'),
+        expect: () => [
+          ExchangeRatesState(
+            status: ExchangeRatesStatus.search,
+            exchangeRates: [aud, usd],
+            filteredList: [usd],
+          ),
+        ],
+      );
+    });
+    group('clearSearch', () {
+      blocTest<ExchangeRatesCubit, ExchangeRatesState>(
+        'emits state with empty filtered list',
+        build: () => buildCubit(),
+        act: (cubit) => cubit.clearSearch(),
+        expect: () => const [
+          ExchangeRatesState(
+            filteredList: [],
+          ),
+        ],
+      );
+    });
+  });
+}
